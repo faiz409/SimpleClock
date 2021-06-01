87b07b4ec0c3beb8534db612486a9ccc65f5316f
Faiz Anwar <faizanwar409@gmail.com>
2021-06-01 16:01:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function updateClock() {
+    // Get the current date
+    let currentTime = new Date();
+
+    // Extract hour, minute and second from the date
+    let currentHour = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+
+    // Pad 0 if minute or second in less than 10(sigle date).
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+    // Convert railway clock to AM/PM clock.
+    currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
+    currentHour = (currentTime == 0) ? 12 : currentHour;
+
+    // Choose AM/PM as per the time of the day.
+    let timeOfDay = (currentHour < 12) ? "AM" : "PM";
+
+    // Prepare the time string from hours, minutes and seconds.
+    let currentTimeStr = currentHour + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+    //  Insert the time string inside the DOM.
+    document.getElementById('clock').innerHTML = currentTimeStr;
+}
